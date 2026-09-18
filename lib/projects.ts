763b6f44872c3e572dc5fa2a28bd3426860b8db0
Diya Chakraborti <chakdiya@gmail.com>
2026-09-18 01:04:56 -0700

    Tweak Visa Protect description wording

--- lib/projects.ts
+++ lib/projects.ts
@@ -35,7 +35,7 @@ export const projects: Project[] = [
   {
     title: 'Visa Protect – Fraud Investigation Tool',
     meta: '2024',
-    description: 'Designed the graph interface from 0-1 for small fintechs, now piloting in 6 countries',
+    description: 'Designing the graph interface from 0-1, now piloting to small fintechs in 6 countries',
     href: 'https://visa-protect-demo.vercel.app/',
     backgroundImage: '/projects/project-2-background.webp',
     mediaOverlayImage: '/projects/project-2-mockup.png',
